95b586da57d9bd9c08bb0bebf2cc031e10e138ee
ronaldoqueiroz80 <ronaldoqueirozsilva80@gmail.com>
2022-08-21 17:35:44 -0300

    update

--- Modulo_2/06-imc/js/script.js
+++ Modulo_2/06-imc/js/script.js
@@ -1,1 +1,11 @@
-console.log('Teste no console do JavaScript');
+function star() {
+  console.log('start');
+
+  console.log(calculaterImc(98, 1.78));
+  console.log(calculaterImc(90, 1.85));
+}
+
+function calculaterImc(weight, height) {
+  return weight / (height * height);
+}
+star();
